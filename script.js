11fbf60d6616843e44d8fc9b7f179cb7986e417e
Marie-Ange-L <76390117+Marie-Ange-L@users.noreply.github.com>
2024-02-26 12:19:15 +0100

    Added headings update / animation

--- script.js
+++ script.js
@@ -29,15 +29,55 @@ const footer = document.querySelector("footer");
 window.addEventListener("scroll", fixNav);
 
 function fixNav() {
-    if (window.scrollY > nav.offsetHeight + 150) {
-        nav.classList.add("active");
-        if (nav.classList.contains("active")) {
-            footer.classList.add("fixed");
-        } else {
-            footer.classList.remove("fixed");
-        }
-    } else {
-        nav.classList.remove("active");
-        footer.classList.remove("fixed");
-    }
+	if (window.scrollY > nav.offsetHeight + 150) {
+		nav.classList.add("active");
+		if (nav.classList.contains("active")) {
+			footer.classList.add("fixed");
+		} else {
+			footer.classList.remove("fixed");
+		}
+	} else {
+		nav.classList.remove("active");
+		footer.classList.remove("fixed");
+	}
 }
+
+// h1 update
+const enHeading = document.querySelector("h1.en_lang");
+const frHeading = document.querySelector("h1.fr_lang");
+
+const enTexts = [
+	"Front-End <span class='highlight'>Developer</span>",
+	"<span class='highlight'>React</span> Enthusiast",
+	"Digital <span class='highlight'>Entrepreneur</span>",
+	"<span class='highlight'>Websites</span> Creator",
+];
+
+const frTexts = [
+	"<span class='highlight'>Développeuse</span> Front-end",
+	"Spécialiste <span class='highlight'>React</span>",
+	"<span class='highlight'>Entrepreneuse</span> numérique",
+	"Créatrice de <span class='highlight'>sites web</span>",
+];
+
+let currentTextIndex = 0;
+
+function updateText() {
+	const enUnderline = enHeading.querySelector(".underline");
+	const frUnderline = frHeading.querySelector(".underline");
+
+	enUnderline.classList.add("hidden");
+	frUnderline.classList.add("hidden");
+
+	setTimeout(() => {
+		enUnderline.innerHTML = enTexts[currentTextIndex];
+		frUnderline.innerHTML = frTexts[currentTextIndex];
+
+		enUnderline.classList.remove("hidden");
+		frUnderline.classList.remove("hidden");
+		currentTextIndex = (currentTextIndex + 1) % enTexts.length;
+	}, 500);
+}
+
+updateText();
+setInterval(updateText, 3000);
